65ae6f80189912922ac706ff5d4588ff5f3cd14e
30parth <aparth3010@gmail.com>
2026-07-22 23:42:29 +0530

    create a bill list api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BillController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,6 @@ Route::middleware('auth:sanctum')->group(function () {
     // Route::post('/products', [ProductController::class, 'store'])->name('api.products.store');
 
     Route::resource('products', ProductController::class);
+    Route::resource('bills', BillController::class);
+
 });
